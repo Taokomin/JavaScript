2839ef1f177e4ab2afcda12badb2b8b3e6cd1ad1
Taokomin <s6206021621015@email.kmuntb.ac.th>
2022-12-28 10:28:40 +0700

    update

--- Home_02/findPerimeter.js
+++ Home_02/findPerimeter.js
@@ -1,7 +1,13 @@
-function findPerimeter(length, width){
-    result = (length + width)*2;
-    return console.log(result);
-  }
+// function findPerimeter(length, width){
+//     result = (length + width)*2;
+//     return console.log(result);
+//   }
+
+// findPerimeter(6,7);
+// findPerimeter(20,10);
+// findPerimeter(2,9);
+
+const findPerimeter = (length, width) => console.log((length + width)*2) ;
 
 findPerimeter(6,7);
 findPerimeter(20,10);
