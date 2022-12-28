7f4737cd5f189beee09a5eeecb8fcff5d3c57bd0
Taokomin <s6206021621015@email.kmuntb.ac.th>
2022-12-28 09:40:04 +0700

    update

---
+++ Lecture_02/reverse.js
@@ -0,0 +1,10 @@
+function reverseString(value){
+
+    value.split("").array.forEach((char) => {
+        reversedValue = char + reversedValue;        
+    });
+
+    return reversedValue;
+}
+
+console.log(reverseString("Reverse Me"));
