56f316c56974e4a8e7a166e65889add48e5705d9
Taokomin <s6206021621015@email.kmutnb.ac.th>
2023-02-08 10:21:37 +0700

    update

--- MID/totalVolume.js
+++ MID/totalVolume.js
@@ -8,5 +8,5 @@ function totalVolume(...boxes) {
   }
   console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1])); // Output: 63
   console.log(totalVolume([2, 2, 2], [2, 1, 1])); // Output: 10
-  console.log(totalVolume([1, 1, 1])); // Output: 1 fgd
+  console.log(totalVolume([1, 1, 1])); // Output: 1
   
